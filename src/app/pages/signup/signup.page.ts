d128077335517b3d48eeae9bce7cc60a7933d443
Robert Collier <robertcdev@gmail.com>
2019-11-10 18:21:02 -0800

    Complete signup page UI

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  goHome() {
+    this.router.navigateByUrl('/home');
+  }
+
+  goLogin() {
+    this.router.navigateByUrl('/login');
   }
 
 }
