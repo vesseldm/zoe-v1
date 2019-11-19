5d64bcfe04feb81199aac9aedb20a1569edc7538
Robert Collier <robertcdev@gmail.com>
2019-11-19 15:21:49 -0800

    Move auth functions from the pages to auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,22 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  doRegister(value){
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
+      .then(
+        res => resolve(res),
+        err => reject(err))
+    })
+  }
+ 
+   doLogin(value){
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(value.email, value.password)
+      .then(
+        res => resolve(res),
+        err => reject(err))
+    })
+  }
 }
