22976f22bbded94b1ff0ebcf613d66c6cd802a6c
Robert Collier <robertcdev@gmail.com>
2019-11-19 20:06:33 -0800

    Build auth service for facebook and user model

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import * as firebase from 'firebase/app';
+import { Platform } from '@ionic/angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Facebook } from '@ionic-native/facebook/ngx';
+import { FirebaseUserModel } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,9 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class AuthService {
 
   constructor(
-    public afAuth: AngularFireAuth
+    public afAuth: AngularFireAuth,
+    public fb: Facebook,
+    public platform: Platform
   ) { }
 
   doRegister(value){
@@ -26,6 +31,39 @@ export class AuthService {
       .then(
         res => resolve(res),
         err => reject(err))
+    })
+  }
+
+  doFacebookLogin(){
+    return new Promise<FirebaseUserModel>((resolve, reject) => {
+      if (this.platform.is('cordova')) {
+        //["public_profile"] is the array of permissions, you can add more if you need
+        this.fb.login(["public_profile"])
+        .then((response) => {
+          const facebookCredential = firebase.auth.FacebookAuthProvider.credential(response.authResponse.accessToken);
+          firebase.auth().signInWithCredential(facebookCredential)
+            .then(user => resolve());
+        }, err => reject(err)
+        );
+      }
+      else {
+        this.afAuth.auth
+        .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+        .then(result => {
+          //Default facebook img is too small and we need a bigger image
+          var bigImgUrl = "https://graph.facebook.com/" + result.additionalUserInfo.profile + "/picture?height=500";
+          // update profile to save the big fb profile img.
+          firebase.auth().currentUser.updateProfile({
+            displayName: result.user.displayName,
+            photoURL: bigImgUrl
+          }).then(res => resolve()
+          ,(err) => {
+            reject(err);
+          });
+        },(err) => {
+          reject(err);
+        })
+      }
     })
   }
 }
