b3b1411fd90a752524ba83e43dd13646bd2e6f54
Robert Collier <robertcdev@gmail.com>
2019-11-20 08:51:11 -0800

    Implement authentication for pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,15 +16,28 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+    public afAuth: AngularFireAuth
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.afAuth.user.subscribe(user => {
+        if (user) {
+          this.router.navigate(["/home"]);
+        } else {
+          this.router.navigate(["/login"]);
+        }
+      }, err => {
+        this.router.navigate(["/login"]);
+      }, () => {
+        this.splashScreen.hide();
+      })
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      // this.splashScreen.hide();
     });
   }
 }
