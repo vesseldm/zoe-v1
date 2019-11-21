73d19779095f663b8ba0b6d6e26eb192f68be2f7
Robert Collier <robertcdev@gmail.com>
2019-11-21 23:57:03 +0700

    integration with my recipes at home page

--- src/app/pages/today/today.page.ts
+++ src/app/pages/today/today.page.ts
@@ -1,28 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-today',
   templateUrl: './today.page.html',
-  styleUrls: ['./today.page.scss'],
+  styleUrls: ['./today.page.scss']
 })
 export class TodayPage implements OnInit {
-
   slideOpts = {
     slidesPerView: 1.7,
     centeredSlides: true,
-    loop: true
+    loop: true,
+    autoHeight: true
   };
+  recipes: any;
+  weekDays: any = [];
+  currentDay: any;
 
-  constructor(
-    public router: Router
-  ) { }
+  constructor(public router: Router, public userService: UserService) {}
 
   ngOnInit() {
+    this.weekDays = [];
+    let curr = new Date();
+    this.currentDay = new Date();
+    let first_day = curr.getDate() - curr.getDay() + 1;
+    for (let i = 0; i < 7; i++) {
+      let next = first_day + i;
+      let next_day = new Date(curr.setDate(next));
+      this.weekDays.push(next_day);
+    }
+
+    this.userService.user$.subscribe(user => {
+      if (user && user.id) {
+        this.userService.getUserPlans(user.id, this.currentDay.toString()).subscribe(plans => {
+          const recipeIds = plans.map(plan => plan.recipeId);
+          this.userService.getPlanedRecipes(recipeIds).subscribe(recipes => {
+            this.recipes = recipes;
+          });
+        });
+      }
+    });
   }
 
   goProfile() {
     this.router.navigateByUrl('/home/profile');
   }
-
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable, of, from } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +10,7 @@ import { switchMap } from 'rxjs/operators';
 export class UserService {
   user$: Observable<any>;
 
-  constructor(
-    public afAuth: AngularFireAuth,
-    public afs: AngularFirestore,
-  ) {
+  constructor(public afAuth: AngularFireAuth, public afs: AngularFirestore) {
     this.user$ = this.afAuth.authState.pipe(
       switchMap(user => {
         if (user) {
@@ -25,4 +22,40 @@ export class UserService {
     );
   }
 
+  getPlanedRecipes(recipeIds): Observable<any> {
+    if (recipeIds.length == 0) return of(null);
+    return this.afs
+      .collection<any>('recipes', ref => ref.where('id', 'in', recipeIds))
+      .snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data();
+            return { ...data };
+          })
+        )
+      );
+  }
+
+  getUserPlans(userId, date): Observable<any> {
+    let curr = new Date(date);
+    let startDate = new Date(curr.getFullYear(), curr.getMonth(), curr.getDate());
+    let endDate = new Date(curr.getFullYear(), curr.getMonth(), curr.getDate() + 1);
+    return this.afs
+      .collection<any>('recipesplan', ref =>
+        ref
+          .where('userId', '==', userId)
+          .where('time', '>=', startDate)
+          .where('time', '<', endDate)
+      )
+      .snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data();
+            return { ...data };
+          })
+        )
+      );
+  }
 }
