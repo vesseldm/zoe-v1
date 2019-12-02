55bb8b0caaa9533bb15b0c3af683d6ebc6d0cb51
Robert Collier <robertcdev@gmail.com>
2019-12-02 13:33:44 +0700

    Admin Analytics Init

--- src/app/admin/analytics/analytics.page.ts
+++ src/app/admin/analytics/analytics.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../services/user.service";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-analytics',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsPage implements OnInit {
 
-  constructor() { }
+  user: any;
+  userBillingEditable: boolean;
+  rewardsEditable: boolean;
+  rewards: any;
+  users: any;
+  silver_users: any;
+  gold_users: any;
+  platinum_users: any;
+
+  constructor(public userService: UserService, public authService: AuthService) { }
 
   ngOnInit() {
-  }
+    this.userService.user$.subscribe(user => {
+      if (user) this.user = user;
+    });
 
+    this.userService.users$.subscribe(users => {
+      this.users = users;
+      this.silver_users = [];
+      this.gold_users = [];
+      this.platinum_users = [];
+
+      users.forEach(item => {
+        if (!item.photoUrl) {
+          item.photoUrl = "https://picsum.photos/id/524/200/300";
+        }
+        switch (item.type) {
+          case "Silver":
+            this.silver_users.push(item);
+            break;
+
+          case "Gold":
+            this.gold_users.push(item);
+            break;
+
+          case "Platinum":
+            this.platinum_users.push(item);
+            break;
+
+          default:
+            break;
+        }
+      });
+    });
+  }
 }
