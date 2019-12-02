3c249ec3d46b69df80daeed2741f68f4c094a39d
Robert Collier <robertcdev@gmail.com>
2019-12-02 13:01:35 +0700

    Admin Settings page

--- src/app/admin/settings/settings.page.ts
+++ src/app/admin/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../services/user.service";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  user: any;
+  userBillingEditable: boolean;
+  userAboutUsEditable: boolean
 
-  constructor() { }
+  constructor(public userService: UserService, public authService: AuthService) { }
 
   ngOnInit() {
+    this.userBillingEditable = false;
+    this.userAboutUsEditable = false;
+
+    this.userService.user$.subscribe(user => {
+      if (user) this.user = user;
+    });
+  }
+
+  doUserBillingEditEnable() {
+    this.userBillingEditable = true
+  }
+
+  doUpdateUserBillingInfo() {
+    try {
+      this.authService.doUpdateUser(this.user);
+      this.userBillingEditable = false
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
+  doUserAboutUsEditEnable() {
+    this.userAboutUsEditable = true;
+  }
+
+  doUpdateAboutUsInfo() {
+    try {
+      this.authService.doUpdateUser(this.user);
+      this.userAboutUsEditable = false
+    } catch (e) {
+      alert(e.message)
+    }
   }
 
 }
