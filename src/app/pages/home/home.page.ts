f06f14c0e771501dbad02c8ce6b4d571fde53a1e
Robert Collier <robertcdev@gmail.com>
2019-12-28 07:57:10 -0800

    Implement header menu automation

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  page_title: string;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  setTitle() {
+    console.log(this.router.url);
+    switch (this.router.url) {
+      case '/home/today':
+        this.page_title = 'Home';
+        break;
+      case '/home/mylist':
+        this.page_title = 'Shopping List';
+        break;
+      case '/home/recipes':
+        this.page_title = 'Recipes';
+        break;
+      case '/home/profile':
+        this.page_title = 'Profile';
+        break;
+      default:
+        this.page_title = 'Home';
+        break;
+    }
   }
 
 }
