c5c9ba5f3c17c3fcbf2c0286525bec805d4926f0
bforsyth1234 <brooks.forsyth@gmail.com>
2020-02-18 16:13:19 -0500

    hosted and connected to heroku.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Use the function app URL you got from the previous step
-  apiUrl: 'https://zoe-api.azurewebsites.net/api'
+  apiUrl: 'https://zoe-dev-api.herokuapp.com'
 };
